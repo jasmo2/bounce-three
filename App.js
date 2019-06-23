c8df635480a06b800a2b71b827113f83fe44e165
jaime <jaime.molina@mattersupply.co>
2019-06-23 11:25:14 -0500

    green dot

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
+import React, { Component } from 'react'
+import { StyleSheet, View } from 'react-native'
 
-import BounceTree from "./lib/";
+import BounceTree from './lib/'
 
 export default class App extends Component {
   render() {
@@ -10,19 +10,20 @@ export default class App extends Component {
         <BounceTree
           delay={200}
           leftDistance={-100}
+          radius={15}
           rightDistance={-150}
           speed={1000}
         />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff"
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
   }
-});
+})
