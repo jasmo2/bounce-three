c0e6d611d1066428b07071797ede406813edd983
jaime <jaime.molina@mattersupply.co>
2019-06-23 10:41:53 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+
+import BounceTree from "./lib/";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <BounceTree
+          delay={200}
+          leftDistance={-100}
+          rightDistance={-150}
+          speed={1000}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff"
+  }
+});
